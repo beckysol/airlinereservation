449e0a60ba4e46ea0740469bf968136e4aa5004c
bereket <bekisol4me@gmail.com>
2021-10-14 09:14:30 +0300

    First Spring boot commit

---
+++ src/test/java/com/example/airlinereservation/AirlinereservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.airlinereservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlinereservationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
